995a9c43753326ae1b41568672adc6d30d24632b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-10 22:17:09 -0300

    Implantando Ticker

---
+++ app/src/main/java/ViniShuet/com/github/android_crypto_monitor/TicketResponse.kt
@@ -0,0 +1,21 @@
+package ViniShuet.com.github.android_crypto_monitor
+
+    //Construtor da classe
+    class TickerResponse(
+        val ticker: Ticker
+    ){
+        //metodos ficaria aqui
+    }
+
+
+    class Ticker(
+        val high: String,
+        val low: String,
+        val vol: String,
+        val last: String,
+        val buy: String,
+        val sell: String,
+        val date: Long
+    ){
+        //metodos ficaria aqui
+    }
